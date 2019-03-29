39bbe6195f8c9ddcfd02862e7bdc1f9f806c8666
Hilary Zen <hzen00@stuy.edu>
2019-03-29 14:35:40 -0400

    Tested add

--- Driver.java
+++ Driver.java
@@ -3,9 +3,12 @@ public class Driver {
     MyLinkedList<Integer> list = new MyLinkedList<Integer>();
     System.out.println(list.size());
 
+    System.out.println(" ");
+
     list.add(1);
     list.add(2);
     list.add(3);
     System.out.println(list.size());
+    System.out.println(list);
   }
 }
