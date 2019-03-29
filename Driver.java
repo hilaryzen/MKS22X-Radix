5221e211c3df8ff9ea397738daf259a418ead67a
Hilary Zen <hzen00@stuy.edu>
2019-03-29 14:32:27 -0400

    Added to Driver

--- Driver.java
+++ Driver.java
@@ -1,6 +1,11 @@
 public class Driver {
   public static void main(String[] args) {
-    MyLinkedList list = new MyLinkedList();
+    MyLinkedList<Integer> list = new MyLinkedList<Integer>();
+    System.out.println(list.size());
+
+    list.add(1);
+    list.add(2);
+    list.add(3);
     System.out.println(list.size());
   }
 }
