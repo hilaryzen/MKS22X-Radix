db487acec05151ef82c1b3b8e7cc7f591de2f7ed
Hilary Zen <hzen00@stuy.edu>
2019-04-01 11:00:32 -0400

    Added sorting by ones digit

--- Radix.java
+++ Radix.java
@@ -4,7 +4,10 @@ public class Radix {
   public static void radixsort(int[] data) {
     ArrayList<MyLinkedList> buckets = new ArrayList<MyLinkedList>();
     for (int i = 0; i < 10; i++) {
-      buckets.add(new MyLinkedList());
+      buckets.add(new MyLinkedList<Integer>());
+    }
+    for (int num = 0; num < data.length; num++) {
+      buckets.get(data[num] % 10).add(data[num]);
     }
     System.out.println(buckets);
   }
