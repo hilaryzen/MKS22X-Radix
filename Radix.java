ee2a5bfe4037f749c21a9c6e35eb09bd323bad84
Hilary Zen <hzen00@stuy.edu>
2019-04-03 22:22:23 -0400

    Finding max

--- Radix.java
+++ Radix.java
@@ -7,6 +7,14 @@ public class Radix {
     MyLinkedList<Integer> numbers = new MyLinkedList<Integer>();
     //System.out.println(numbers);
 
+    //Finding length of longest number
+    int max = Math.abs(data[0]);
+    for (int i = 1; i < data.length; i++) {
+      if (Math.abs(data[i]) > max) {
+        max = data[i];
+      }
+    }
+
     for (int num : data) {
       buckets[num % 10].add(num);
     }
